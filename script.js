21aa15a2cf656e2460aef9db5ea4e468611f3b17
Abdul Kader Jilani <akjilani691995@gmail.com>
2025-05-31 23:13:53 +0600

    2nd commit

--- script.js
+++ script.js
@@ -1,21 +1,20 @@
 function updateClock() {
-    const clock = document.getElementById("clock");
-    const now = new Date();
-  
-    let hours = now.getHours();
-    let minutes = now.getMinutes();
-    let seconds = now.getSeconds();
-    const ampm = hours >= 12 ? 'PM' : 'AM';
-  
-    hours = hours % 12 || 12;
-    hours = String(hours).padStart(2, '0');
-    minutes = String(minutes).padStart(2, '0');
-    seconds = String(seconds).padStart(2, '0');
-  
-    const timeString = `${hours}:${minutes}:${seconds} ${ampm}`;
-    clock.textContent = timeString;
-  }
-  
-  setInterval(updateClock, 1000);
-  updateClock(); // run once immediately
-  
+  const clock=document.getElementById("clock");
+  const now = new Date();
+
+  let hours = now.getHours();
+  let minutes = now.getMinutes();
+  let seconds = now.getSeconds();
+  const ampm = hours >= 12 ? 'PM' : 'AM';
+
+  hours = hours % 12 || 12;
+  hours = String(hours).padStart(2, '0');
+  minutes = String(minutes).padStart(2, 0);
+  seconds = String(seconds).padStart(2, '0');
+
+  const timeString = `${hours}:${minutes}:${seconds} ${ampm}`;
+  clock.textContent = timeString;
+}
+
+setInterval(updateClock, 1000);
+updateClock()
